3e1bfc8f874a053ec5031c13b2380e312419bb4f
Adrian Bartoszek <adrian.bartoszek1@gmail.com>
2021-03-24 18:02:44 +0100

    Zmiana pliku MainActivity.kt

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,99 +2,45 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
-
-
-
+import android.widget.EditText
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var imie : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.oceny_b)
         val imie = findViewById<EditText>(R.id.imie)
         val nazwisko = findViewById<EditText>(R.id.nazwisko)
-        val oceny = findViewById<EditText>(R.id.ocena)
-        val fokus = findViewById<TextView>(R.id.fokus)
+        val liczbaOcen = findViewById<EditText>(R.id.ocena)
+        val przyciskOceny = findViewById<Button>(R.id.oceny_b)
 
-         fun validate_imie() : Boolean{
-            if(imie.text.toString().isEmpty()){
-                imie.error = "Imie nie może być puste!"
-                Toast.makeText(applicationContext,"Imie nie może być puste!",Toast.LENGTH_SHORT).show()
-                return false
-            }
-            else
-                return true
-         }
-        fun validate_nazwisko() : Boolean{
-            if(nazwisko.text.toString().isEmpty()){
-                nazwisko.error = "Nazwisko nie może być puste!"
-                Toast.makeText(applicationContext,"Nazwisko nie może być puste!",Toast.LENGTH_SHORT).show()
-                return false
-            }
-            else
-                return  true
-
-        }
-        fun validate_oceny() : Boolean{
-            if(oceny.text.toString().toInt() in 5..15){
-                return true
-            }
-            else
-            {
-                oceny.error = "Wpisz oceny od 5 do 15!"
-                Toast.makeText(applicationContext,"Wpisz oceny od 5 do 15!",Toast.LENGTH_SHORT).show()
-                return false
+        imie.setOnFocusChangeListener { _, hasFocus ->
+            if(hasFocus) {
+                //Toast.makeText(applicationContext, "Jest fokus", Toast.LENGTH_SHORT).show()
+            } else if(!hasFocus && imie.text.isEmpty()) {
+                Toast.makeText(applicationContext, "Pole imie nie moze byc puste", Toast.LENGTH_SHORT).show()
             }
         }
 
-
-        imie.onFocusChangeListener = View.OnFocusChangeListener{ view, b ->
-            if(b){
-               // Toast.makeText(applicationContext,"jest fokus",Toast.LENGTH_LONG).show()
-            }
-            else{
-
-               // Toast.makeText(applicationContext,"utrata fokusa",Toast.LENGTH_LONG).show()
-                when (validate_imie() && validate_nazwisko() && validate_oceny() ) {
-                    true -> button.setVisibility(View.VISIBLE)
-                    false ->  button.setVisibility(View.INVISIBLE)
-                }
+        nazwisko.setOnFocusChangeListener { _, hasFocus ->
+            if(hasFocus) {
+                //Toast.makeText(applicationContext, "Jest fokus", Toast.LENGTH_SHORT).show()
+            } else if(!hasFocus && imie.text.isEmpty()) {
+                Toast.makeText(applicationContext, "Pole nazwisko nie moze byc puste", Toast.LENGTH_SHORT).show()
             }
         }
 
-        nazwisko.onFocusChangeListener = View.OnFocusChangeListener{view, b ->
-            if(b){
+        liczbaOcen.setOnFocusChangeListener { _, hasFocus ->
+            if(hasFocus) {
+                //Toast.makeText(applicationContext, "Jest fokus", Toast.LENGTH_SHORT).show()
+            } else if(!hasFocus && imie.text.isEmpty()) {
+                Toast.makeText(applicationContext, "Pole liczba ocen nie moze byc puste", Toast.LENGTH_SHORT).show()
             }
-            else{
-                when (validate_nazwisko() && validate_nazwisko() && validate_oceny() ) {
-                    true -> button.setVisibility(View.VISIBLE)
-                    false ->  button.setVisibility(View.INVISIBLE)
-                }
-            }
-        }
-
-        oceny.onFocusChangeListener = View.OnFocusChangeListener{view, b->
-            if(b){
-            }
-            else{
-                when (validate_nazwisko() && validate_nazwisko() && validate_oceny() ) {
-                    true -> button.setVisibility(View.VISIBLE)
-                    false ->  button.setVisibility(View.INVISIBLE)
-                }
-            }
-        }
-
-        button.setOnClickListener{
-            val text = imie.text
-            Toast.makeText(applicationContext,text,Toast.LENGTH_SHORT).show()
         }
     }
 }
