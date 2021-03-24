b5b9b2dfcfa7699860bcdcb975d8b638e59d48ae
BoleQ8 <boleq7@gmail.com>
2021-03-24 17:31:46 +0100

    update

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.Button
-import androidx.core.widget.addTextChangedListener
+
+
 
 
 
@@ -25,46 +24,77 @@ class MainActivity : AppCompatActivity() {
         val oceny = findViewById<EditText>(R.id.ocena)
         val fokus = findViewById<TextView>(R.id.fokus)
 
+         fun validate_imie() : Boolean{
+            if(imie.text.toString().isEmpty()){
+                imie.error = "Imie nie może być puste!"
+                Toast.makeText(applicationContext,"Imie nie może być puste!",Toast.LENGTH_SHORT).show()
+                return false
+            }
+            else
+                return true
+         }
+        fun validate_nazwisko() : Boolean{
+            if(nazwisko.text.toString().isEmpty()){
+                nazwisko.error = "Nazwisko nie może być puste!"
+                Toast.makeText(applicationContext,"Nazwisko nie może być puste!",Toast.LENGTH_SHORT).show()
+                return false
+            }
+            else
+                return  true
+
+        }
+        fun validate_oceny() : Boolean{
+            if(oceny.text.toString().toInt() in 5..15){
+                return true
+            }
+            else
+            {
+                oceny.error = "Wpisz oceny od 5 do 15!"
+                Toast.makeText(applicationContext,"Wpisz oceny od 5 do 15!",Toast.LENGTH_SHORT).show()
+                return false
+            }
+        }
+
 
         imie.onFocusChangeListener = View.OnFocusChangeListener{ view, b ->
             if(b){
-              //  Toast.makeText(applicationContext,"jest fokus",Toast.LENGTH_LONG).show()
+               // Toast.makeText(applicationContext,"jest fokus",Toast.LENGTH_LONG).show()
             }
             else{
+
                // Toast.makeText(applicationContext,"utrata fokusa",Toast.LENGTH_LONG).show()
-                imie.addTextChangedListener(object : TextWatcher {
-
-                    override fun afterTextChanged(s: Editable?) {
-                        val msg: String = imie.text.toString()
-                        
-                    }
-
-                    override fun beforeTextChanged(s: CharSequence, start: Int,
-                                                   count: Int, after: Int) {
-                    }
-
-                    override fun onTextChanged(s: CharSequence, start: Int,
-                                               before: Int, count: Int) {
-                        if(start == 0) {
-                            Toast.makeText(applicationContext,"Uzupełnij imie",Toast.LENGTH_SHORT).show()
-                        }
-
-                    }
-                })
+                when (validate_imie() && validate_nazwisko() && validate_oceny() ) {
+                    true -> button.setVisibility(View.VISIBLE)
+                    false ->  button.setVisibility(View.INVISIBLE)
+                }
             }
-
         }
 
+        nazwisko.onFocusChangeListener = View.OnFocusChangeListener{view, b ->
+            if(b){
+            }
+            else{
+                when (validate_nazwisko() && validate_nazwisko() && validate_oceny() ) {
+                    true -> button.setVisibility(View.VISIBLE)
+                    false ->  button.setVisibility(View.INVISIBLE)
+                }
+            }
+        }
+
+        oceny.onFocusChangeListener = View.OnFocusChangeListener{view, b->
+            if(b){
+            }
+            else{
+                when (validate_nazwisko() && validate_nazwisko() && validate_oceny() ) {
+                    true -> button.setVisibility(View.VISIBLE)
+                    false ->  button.setVisibility(View.INVISIBLE)
+                }
+            }
+        }
 
         button.setOnClickListener{
             val text = imie.text
-
             Toast.makeText(applicationContext,text,Toast.LENGTH_SHORT).show()
-
         }
-
     }
-
-
-
 }
